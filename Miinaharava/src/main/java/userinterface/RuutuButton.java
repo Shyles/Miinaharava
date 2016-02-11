8940669ab012dde88a9162c829e61fe39f931358
Ola Länsman <ola.lansman@helsinki.fi>
2016-02-11 10:17:49 +0200

    En ole ihan varma mitä tapahtuu.

---
+++ Miinaharava/src/main/java/userinterface/RuutuButton.java
@@ -0,0 +1,15 @@
+
+package userinterface;
+
+import javax.swing.JButton;
+import miinaharava.gameObjects.Ruutu;
+
+
+public class RuutuButton extends JButton {
+    
+    private Ruutu ruutu;
+
+    public RuutuButton(Ruutu ruutu) {
+        this.ruutu = ruutu;
+    }
+}
---
+++ Miinaharava/src/test/java/miinaharava/PerfectGameTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miinaharava;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Olaml
+ */
+public class PerfectGameTest {
+
+    GameBoard gb = new GameBoard();
+    int[][] mines = {{0, 0}, {1, 2}, {0, 4}, {3, 0}, {1, 1}};
+
+    public PerfectGameTest() {
+
+    }
+
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
